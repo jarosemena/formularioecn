fbe588a205cb17e67e0c9a11d9f9c1cf4eb8984c
Jose Arosemena <jjarosemena@gmail.com>
2019-12-15 22:47:03 -0500

    este commit aun no funciona el primer componente pero esta la base de la app de react

---
+++ formularioecn/src/components/Formulario.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Formulario extends React.Component {
+    render(){
+       return <h1>Prueba de Titulo </h1>
+    }
+}
+
+export default Formulario;
--- formularioecn/src/index.js
+++ formularioecn/src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import Formulario from './components/Formulario'
+
+const container = document.getElementById('app');
+
+ReactDOM.render (<Formulario />,container);
