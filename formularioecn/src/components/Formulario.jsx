172333109cbbfbd568b7ccdf947409eb61487204
Jose Jair <jjarosemena@gmail.com>
2020-03-15 23:14:16 -0500

    correcciones menores se inicia el proceso para crear rutina de guardar

--- formularioecn/src/components/Formulario.jsx
+++ formularioecn/src/components/Formulario.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import "../styles/Formulario.css"
 import 'bootstrap/dist/css/bootstrap.css';
-
+import {Conf} from "../config/DataConfig.json"
 
 
 
 class Formulario extends React.Component {
 
     state = {};
+   
 
     handleChange = (e) => {
         console.log({ name: e.target.name, value: e.target.value, });
@@ -17,6 +18,7 @@ class Formulario extends React.Component {
     }
     handleClick = (e) => {
         console.log(this.state);
+        
 
     }
     handleSubmit = (e) => {
@@ -25,8 +27,12 @@ class Formulario extends React.Component {
     }
     handleLoad()  
     {
-       console.log("tipopersona=" + this.props.TipoPesrona)
-            this.setState({ TipoPesrona: this.props.TipoPesrona, cedula: this.props.Cedula});
+        if (this.state.TipoPesrona === undefined) {
+            
+            this.setState({ TipoPesrona: this.props.TipoPesrona});
+            this.setState({ cedula: this.props.Cedula});
+            }else 
+            {console.log("estado " + this.state.TipoPesrona)}
     }
 
     render() {
@@ -46,19 +52,19 @@ class Formulario extends React.Component {
                         <div className="row">
 
                             <div className="col-form-label-sm col-6 col-md-3 col-lg-3" id="lbnombre">
-                                <label for="exampleFormControlInput1">1er Nombre</label>
+                                <label >1er Nombre</label>
                                 <input onChange={this.handleChange} type="text" className="form-control" name="Nombre" value={this.state.Nombre} placeholder="Nombre"></input>
                             </div>
                             <div className="col-form-label-sm col-6 col-md-3 col-lg-3" id="lbnombre">
-                                <label for="exampleFormControlInput1">2do Nombre</label>
+                                <label >2do Nombre</label>
                                 <input onChange={this.handleChange} type="text" className="form-control" name="Nombre2" value={this.state.Nombre2} placeholder="Nombre2"></input>
                             </div>
                             <div className="col-form-label-sm col-6 col-md-3 col-lg-3" id="lbnombre">
-                                <label for="exampleFormControlInput1">1er Apellido</label>
+                                <label >1er Apellido</label>
                                 <input onChange={this.handleChange} type="text" className="form-control" name="apellido" value={this.state.apellido} placeholder="Apellido"></input>
                             </div>
                             <div className="col-form-label-sm col-6 col-md-3 col-lg-3" id="lbnombre">
-                                <label for="exampleFormControlInput1">2do Apellido</label>
+                                <label >2do Apellido</label>
                                 <input onChange={this.handleChange} type="text" className="form-control" name="apellido2" value={this.state.apellido2} placeholder="Apellido2"></input>
                             </div>
 
@@ -69,22 +75,22 @@ class Formulario extends React.Component {
                         </div>
                         <div className="row">
                             <div className="col-form-label-sm col-6 col-md-3 col-lg-3" id="lbnombre">
-                                <label for="exampleFormControlInput1">Edad</label>
+                                <label >Edad</label>
                                 <input onChange={this.handleChange} type="text" className="form-control" name="edad" value={this.state.edad} placeholder="Edad"></input>
 
                             </div>
                             <div className="col-form-label-sm col-6 col-md-3 col-lg-3" id="lbnombre">
-                                <label for="exampleFormControlInput1">Cédula</label>
+                                <label >Cédula</label>
                                 <input onChange={this.handleChange} type="text" className="form-control" name="cedula" value={this.state.cedula} placeholder="Cedula"></input>
 
                             </div>
                             <div className="col-form-label-sm col-6 col-md-3 col-lg-3" id="lbnombre">
-                                <label for="exampleFormControlInput1">Fecha Nacimiento</label>
+                                <label >Fecha Nacimiento</label>
                                 <input onChange={this.handleChange} type="date" className="form-control" name="nacimiento" value={this.state.nacimiento} placeholder="Fecha Nacimiento"></input>
 
                             </div>
                             <div className="col-form-label-sm col-6 col-md-3 col-lg-3" id="lbnombre">
-                                <label for="exampleFormControlInput1">Nacionalidad</label>
+                                <label >Nacionalidad</label>
                                 <input onChange={this.handleChange} type="text" className="form-control" name="nacionalidad" value={this.state.nacionalidad} placeholder="Nacionalidad"></input>
 
                             </div>
@@ -95,17 +101,20 @@ class Formulario extends React.Component {
 
                         <div className="row" >
                             <div className="col-form-label-sm col-6 col-md-3 col-lg-3" id="lbnombre">
-                                <label for="exampleFormControlInput1">Celular</label>
+                                <label >Celular</label>
                                 <input onChange={this.handleChange} type="Number" className="form-control" name="Celular" value={this.state.Celular} placeholder="Celular"></input>
 
                             </div>
+                            {/*
                             <div className="col-form-label-sm col-6 col-md-3 col-lg-3" id="lbnombre">
-                                <label for="exampleFormControlInput1">Tel. Casa</label>
+                                <label >Tel. Casa</label>
                                 <input onChange={this.handleChange} type="Number" className="form-control" name="Tel" value={this.state.Tel} placeholder="Telefono"></input>
 
                             </div>
-                            <div className="col-form-label-sm col-12 col-md-6 col-lg-6" id="lbnombre">
-                                <label for="exampleFormControlInput1">Dirección</label>
+                            */}
+
+                            <div className="col-form-label-sm col-12 col-md-9 col-lg-9" id="lbnombre">
+                                <label >Dirección</label>
                                 <input onChange={this.handleChange} type="text" className="form-control" name="Dirección" value={this.state.Dirección} placeholder="Direccion"></input>
 
                             </div>
@@ -115,7 +124,7 @@ class Formulario extends React.Component {
 
 
                             <div className="col-form-label-sm  col-8 col-md-6 col-lg-6" id="lbnombre">
-                                <label for="exampleFormControlInput1">Correo Electronico</label>
+                                <label >Correo Electronico</label>
                                 <div className="input-group mb-2 mr-sm-2">
                                     <div className="input-group-prepend">
                                         <div className="input-group-text">@</div>
@@ -124,7 +133,7 @@ class Formulario extends React.Component {
                                 </div>
                             </div>
                             <div className="col-form-label-sm col-4 col-md-6 col-lg-6" id="lbnombre">
-                                <label for="exampleFormControlInput1">Ocupación</label>
+                                <label >Ocupación</label>
                                 <input onChange={this.handleChange} type="text" className="form-control" name="Ocupación" value={this.state.Ocupación} placeholder="Ocupación"></input>
 
                             </div>
@@ -132,25 +141,25 @@ class Formulario extends React.Component {
                         </div>
                         <div className="row">
                             <div className="col-form-label-sm col-12" id="lbnombre">
-                                <label for="exampleFormControlInput1">Contacto Familiar Padre o Madre</label>
+                                <label >Contacto Familiar Padre o Madre</label>
                             </div>
                         </div>
 
                         <div className="row">
 
                             <div className="col-form-label-sm col-5 col-md-5 col-lg-3" id="lbnombre">
-                                <label for="exampleFormControlInput1">Nombre</label>
+                                <label >Nombre</label>
                                 <input onChange={this.handleChange} type="Text" className="form-control" name="Contacto" value={this.state.Contacto} placeholder="Nombre"></input>
 
                             </div>
                             <div className="col-form-label-sm col-6 col-md-6 col-lg-2" id="lbnombre">
 
-                                <label for="exampleFormControlInput1">Celular</label>
+                                <label >Celular</label>
                                 <input onChange={this.handleChange} type="Number" className="form-control" name="CelularContacto" value={this.state.CelularContacto} placeholder="Celular"></input>
 
                             </div>
                             <div className="col-form-label-sm  col-6 col-md-6 col-lg-3" id="lbnombre">
-                                <label for="exampleFormControlInput1">Correo Electronico</label>
+                                <label >Correo Electronico</label>
                                 <div className="input-group mb-2 mr-sm-2">
                                     <div className="input-group-prepend">
                                         <div className="input-group-text">@</div>
@@ -160,28 +169,19 @@ class Formulario extends React.Component {
 
                             </div>
                             <div className="col-form-label-sm col-6 col-md-6 col-lg-3 " id="lbnombre">
-                                <label for="exampleFormControlInput1">   Vive en El extranjero o en el Interior del país </label>
+                                <label >   Vive en El extranjero o en el Interior del país </label>
                                 <div className="form-check form-check-inline">
-                                    <input onChange={this.handleChange} className="form-check-input" type="radio" name="inlineRadioOptions1" id="extranjeroRadio1" value="option1"></input>
+                                    <input onChange={this.handleChange} className="form-check-input" type="radio" name="inlineRadioOptions1" value="option1"></input>
                                     <label className="form-check-label" for="inlineRadio1">Sí</label>
                                 </div>
                                 <div className="form-check form-check-inline">
-                                    <input onChange={this.handleChange} className="form-check-input" type="radio" defaultChecked name="inlineRadioOptions1" id="extranjeroRadio2" value="option2"></input>
+                                    <input onChange={this.handleChange} className="form-check-input" type="radio" defaultChecked name="inlineRadioOptions1" value="option2"></input>
                                     <label className="form-check-label" for="inlineRadio2">No</label>
                                 </div>
 
                             </div>
                         </div>
-                        <div className="row justify-contend-md-center"  >
-                            <div className="col col-2">
-                            </div>
-                            <div className="col col-8">
-                                <button onClick={this.handleClick} type="button" className="btn btn-primary btn-block">Guardar</button>
-                            </div>
-                            <div className="col col-2">
-                            </div>
-                        </div>
-                  
+                        <button type="button" onClick={this.handleClick} className="btn btn-primary btn-block">Guardar</button>
 
                 
 
--- formularioecn/src/components/FormularioDetalle.jsx
+++ formularioecn/src/components/FormularioDetalle.jsx
@@ -25,9 +25,13 @@ class Formulario extends React.Component {
     }
     handleLoad()  
     {
-       
-        this.setState({ TipoPesrona: this.props.TipoPesrona});
-        this.setState({ cedula: this.props.Cedula});
+              
+        if (this.state.TipoPesrona === undefined) {
+            
+            this.setState({ TipoPesrona: this.props.TipoPesrona});
+            this.setState({ cedula: this.props.Cedula});
+            }else 
+            {console.log("estado " + this.state.TipoPesrona)}
     }
 
 
@@ -50,25 +54,25 @@ class Formulario extends React.Component {
                         </div>
                         <div className="row">
                             <div className="col-form-label-sm col-6 col-md-6 col-lg-4" id="lbnombre">
-                                <label for="exampleFormControlInput1">Religión</label>
+                                <label >Religión</label>
                                 <input onChange={this.handleChange} type="Text" className="form-control" name="Religión" value={this.state.Religión} placeholder="Religión"></input>
 
                             </div>
                             <div className="col-form-label-sm col-6 col-md-6 col-lg-4" id="lbnombre">
 
-                                <label for="exampleFormControlInput1">Parroquia a la que pertenece</label>
+                                <label >Parroquia a la que pertenece</label>
                                 <input onChange={this.handleChange} type="Number" className="form-control" name="Parroquia" value={this.state.Parroquia} placeholder="Parroquia"></input>
 
                             </div>
 
                             <div className="col-form-label-sm col-12 col-md-6 col-lg-4 " id="lbnombre">
-                                <label for="exampleFormControlInput1"> ¿ Tiene parientes o amigos que han vivido el Encuentro de Novios ?</label>
+                                <label > ¿ Tiene parientes o amigos que han vivido el Encuentro de Novios ?</label>
                                 <div className="form-check form-check-inline">
-                                    <input onChange={this.handleChange} className="form-check-input" type="radio" name="inlineRadioOptions2" id="parientesRadio1" value="option1"></input>
+                                    <input onChange={this.handleChange} className="form-check-input" type="radio" name="inlineRadioOptions2" value="option1"></input>
                                     <label className="form-check-label" for="inlineRadio1">Sí</label>
                                 </div>
                                 <div className="form-check form-check-inline">
-                                    <input onChange={this.handleChange} className="form-check-input" type="radio" defaultChecked name="inlineRadioOptions2" id="parientesRadio2" value="option2"></input>
+                                    <input onChange={this.handleChange} className="form-check-input" type="radio" defaultChecked name="inlineRadioOptions2"  value="option2"></input>
                                     <label className="form-check-label" for="inlineRadio2">No</label>
                                 </div>
 
@@ -82,13 +86,13 @@ class Formulario extends React.Component {
                         </div>
                         <div className="row">
                             <div className="col-form-label-sm col-6 col-md-6 col-lg-3" id="lbnombre">
-                                <label for="exampleFormControlInput1"> ¿ Ha estado casado por la iglesia ?</label>
+                                <label > ¿ Ha estado casado por la iglesia ?</label>
                                 <div className="form-check form-check-inline">
-                                    <input onChange={this.handleChange} className="form-check-input" type="radio" name="inlineRadioOptions4" id="CasadoRadio1" value="option1"></input>
+                                    <input onChange={this.handleChange} className="form-check-input" type="radio" name="inlineRadioOptions4"  value="option1"></input>
                                     <label className="form-check-label" for="inlineRadio1">Sí</label>
                                 </div>
                                 <div className="form-check form-check-inline">
-                                    <input onChange={this.handleChange} className="form-check-input" type="radio" defaultChecked name="inlineRadioOptions4" id="CasadoRadio2" value="option2"></input>
+                                    <input onChange={this.handleChange} className="form-check-input" type="radio" defaultChecked name="inlineRadioOptions4" value="option2"></input>
                                     <label className="form-check-label" for="inlineRadio2">No</label>
                                 </div>
 
@@ -97,18 +101,18 @@ class Formulario extends React.Component {
 
                             <div className="col-form-label-sm col-6 col-md-6 col-lg-3" id="lbnombre">
 
-                                <label for="exampleFormControlInput1">Cuantos tiempo <input type="Number" className="form-control" name="TiempoCasdoIglesia" value={this.state.TiempoCasdoIglesia} placeholder="..."></input></label>
+                                <label >Cuantos tiempo <input type="Number" className="form-control" name="TiempoCasdoIglesia" value={this.state.TiempoCasdoIglesia} placeholder="..."></input></label>
 
 
                             </div>
                             <div className="col-form-label-sm col-6 col-md-6 col-lg-3 " id="lbnombre">
-                                <label for="exampleFormControlInput1"> ¿    Mantiene union de Hecho     ?</label>
+                                <label > ¿    Mantiene union de Hecho     ?</label>
                                 <div className="form-check form-check-inline">
-                                    <input onChange={this.handleChange} className="form-check-input" type="radio" name="inlineRadioOptions" id="UnidoRadio1" value="option1"></input>
+                                    <input onChange={this.handleChange} className="form-check-input" type="radio" name="inlineRadioOptions"  value="option1"></input>
                                     <label className="form-check-label" for="inlineRadio1">Sí</label>
                                 </div>
                                 <div className="form-check form-check-inline">
-                                    <input onChange={this.handleChange} className="form-check-input" type="radio" defaultChecked name="inlineRadioOptions" id="UnidoRadio2" value="option2"></input>
+                                    <input onChange={this.handleChange} className="form-check-input" type="radio" defaultChecked name="inlineRadioOptions"  value="option2"></input>
                                     <label className="form-check-label" for="inlineRadio2">No</label>
                                 </div>
 
@@ -117,7 +121,7 @@ class Formulario extends React.Component {
 
                             <div className="col-form-label-sm col-6 col-md-6 col-lg-3" id="lbnombre">
 
-                                <label for="exampleFormControlInput1">Cuanto Tiempo <input onChange={this.handleChange} type="Number" className="form-control" name="TiempoUnion" value={this.state.TiempoUnion} placeholder="..."></input></label>
+                                <label >Cuanto Tiempo <input onChange={this.handleChange} type="Number" className="form-control" name="TiempoUnion" value={this.state.TiempoUnion} placeholder="..."></input></label>
 
 
                             </div>
@@ -134,19 +138,19 @@ class Formulario extends React.Component {
                         <div className="row">
                             <div className="col-form-label-sm col-4 col-md-4 " id="lbnombre">
 
-                                <label for="exampleFormControlInput1">Nombre</label>
+                                <label >Nombre</label>
                                 <input onChange={this.handleChange} type="Text" className="form-control" name="ERNombre" value={this.state.ERNombre} placeholder="Nombre"></input>
 
                             </div>
                             <div className="col-form-label-sm col-4 col-md-4 " id="lbnombre">
 
-                                <label for="exampleFormControlInput1">Parentensco</label>
+                                <label >Parentensco</label>
                                 <input onChange={this.handleChange} type="Text" className="form-control" name="ERparentesco" value={this.state.ERparentesco} placeholder="Parentesco"></input>
 
                             </div>
                             <div className="col-form-label-sm col-4 col-md-4 " id="lbnombre">
 
-                                <label for="exampleFormControlInput1">Celular</label>
+                                <label >Celular</label>
                                 <input onChange={this.handleChange} type="Number" className="form-control" name="ERCelular" value={this.state.ERCelular} placeholder="Celular"></input>
 
                             </div>
@@ -154,19 +158,19 @@ class Formulario extends React.Component {
                         </div>
                         <div className="row">
                             <div className="col-form-label-sm col-8 col-md-6 col-lg-5 col-xl-4" id="lbnombre">
-                                <label for="exampleFormControlInput1">Alérgico (a) a algún alimentación/Medicamento </label>
+                                <label >Alérgico (a) a algún alimentación/Medicamento </label>
                                 <div className="form-check form-check-inline">
-                                    <input onChange={this.handleChange} className="form-check-input" type="radio" name="inlineRadioOptions0" id="AlergiaRadio1" value="option1"></input>
+                                    <input onChange={this.handleChange} className="form-check-input" type="radio" name="inlineRadioOptions0"  value="option1"></input>
                                     <label className="form-check-label" for="inlineRadio1">Sí</label>
                                 </div>
                                 <div className="form-check form-check-inline">
-                                    <input onChange={this.handleChange} className="form-check-input" type="radio" defaultChecked name="inlineRadioOptions0" id="AlergiaRadio2" value="option2"></input>
+                                    <input onChange={this.handleChange} className="form-check-input" type="radio" defaultChecked name="inlineRadioOptions0" value="option2"></input>
                                     <label className="form-check-label" for="inlineRadio2">No</label>
                                 </div>
 
                             </div>
                             <div className="col-form-label-sm col-4 col-md-6 col-lg-7 col-xl-8" id="lbnombre">
-                                <label for="exampleFormControlInput1">Detalle</label>
+                                <label >Detalle</label>
                                 <input onChange={this.handleChange} type="text" className="form-control" name="Alergias" value={this.state.Alergias} placeholder="Alergias"></input>
 
                             </div>
@@ -186,13 +190,13 @@ class Formulario extends React.Component {
 
 
                             <div className="col-form-label-sm col-12 col-md-6 col-lg-6 " id="lbnombre">
-                                <label for="exampleFormControlInput1"> ¿ Necesita constancia de asistencia para la empresa en la que labora ?</label>
+                                <label > ¿ Necesita constancia de asistencia para la empresa en la que labora ?</label>
                                 <div className="form-check form-check-inline">
-                                    <input onChange={this.handleChange} className="form-check-input" type="radio" name="inlineRadioOptions6" id="ConstanciaRadio1" value="option1"></input>
+                                    <input onChange={this.handleChange} className="form-check-input" type="radio" name="inlineRadioOptions6"  value="option1"></input>
                                     <label className="form-check-label" for="inlineRadio1">Sí</label>
                                 </div>
                                 <div className="form-check form-check-inline">
-                                    <input onChange={this.handleChange} className="form-check-input" type="radio" defaultChecked name="inlineRadioOptions6" id="ConstanciaRadio2" value="option2"></input>
+                                    <input onChange={this.handleChange} className="form-check-input" type="radio" defaultChecked name="inlineRadioOptions6"  value="option2"></input>
                                     <label className="form-check-label" for="inlineRadio2">No</label>
                                 </div>
 
@@ -200,13 +204,13 @@ class Formulario extends React.Component {
 
                             <div className="col-form-label-sm col-6 col-md-3 col-lg-3" id="lbnombre">
 
-                                <label for="exampleFormControlInput1">Nombre de la Empresa</label>
+                                <label >Nombre de la Empresa</label>
                                 <input onChange={this.handleChange} type="Text" className="form-control" name="Empresa" value={this.state.Empresa} placeholder="Empresa"></input>
 
                             </div>
                             <div className="col-form-label-sm col-6 col-md-3 col-lg-3" id="lbnombre">
 
-                                <label for="exampleFormControlInput1">a quien va dirigida</label>
+                                <label >a quien va dirigida</label>
                                 <input onChange={this.handleChange} type="text" className="form-control" name="Dirigidoa" value={this.state.Dirigidoa} placeholder="Sr."></input>
 
                             </div>
